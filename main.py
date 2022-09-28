51c9fe8d57008b27857d8f9e0272fedfb94e4154
Stefan Wijnja (stfwn) <stefan@stfwn.com>
2022-09-28 13:14:56 +0200

    Complete pipeline including FAISS

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sentence_transformers import SentenceTransformer, util
 import config
 from data import MSMarcoDocs
 import encoding
-from model import Model
 import preprocessing
 
 
@@ -20,9 +19,7 @@ def main():
     docs = ms_marco_docs.get_docs()
 
     if config.stage == "DEV":
-        docs = docs.train_test_split(train_size=10000, test_size=1, shuffle=True)[
-            "train"
-        ]
+        docs = docs.train_test_split(train_size=10, test_size=1, shuffle=True)["train"]
 
     docs = docs.map(
         preprocessing.doc_to_passages,
@@ -48,43 +45,16 @@ def main():
         keep_in_memory=config.use_cache,
     )
 
-    docs.add_faiss_index(column="embedding")
-    docs.save_faiss_index("embeddings", "./data/ms-marco/document-embeddings.faiss")
+    try:
+        docs.load_faiss_index("embedding", "./data/ms-marco/document-embeddings.faiss")
+    except:
+        docs.add_faiss_index(column="embedding")
+        docs.save_faiss_index("embedding", "./data/ms-marco/document-embeddings.faiss")
 
-    ##
-
-    # example_doc = [
-    #    "Around 9 Million people live in London",
-    #    "London is known for its financial district",
-    #    "Bangkok is the capital of Thailand",
-    #    "The new macbook M2 line is the best of the best",
-    # ]
-
-    # example_embeddings = model.encode_passages(example_doc)
-    # model.cache_embedding(example_embeddings, "doc_0")
-
-    ## (3) Build FAISS index
-    # faiss_index = FaissIndexer()
-    # faiss_index.set_embeddings(example_embeddings)
-    # faiss_index.create_index("L2")
-
-    ## Some example cases using the example docs
-    # example_sentence = "What is the capital of Thailand"
-    # example_query = model.encode_query(example_sentence)
-    # D, I = faiss_index.search(example_query, k=1)
-    # print(example_sentence)
-    # print(example_doc[I[0][0]])
-    # print("\n")
-
-    # example_sentence = "I live in the capital of England"
-    # example_query = model.encode_query(example_sentence)
-    # D, I = faiss_index.search(example_query, k=4)
-    # ranking = [example_doc[I[0][i]] for i in I[0]]
-    # print(example_sentence)
-    # print(ranking)
-
-    # (4) Compute score on dev set
-    raise NotImplementedError
+    # Score a query
+    scores, retrieved_docs = docs.get_nearest_examples(
+        "embedding", model.encode("Here's an example query"), k=10
+    )
 
 
 if __name__ == "__main__":
