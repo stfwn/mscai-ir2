34e83d9a0e3ad3169e3ee4a213e161f77e549fe0
Thijs Sluijter <thijssluijter@sp-byods-145-109-28-20.wireless.uva.nl>
2022-10-19 14:01:50 +0200

    final

--- score_aggregation.py
+++ score_aggregation.py
@@ -2,22 +2,17 @@ from typing import Callable
 from argparse import ArgumentParser
 import os
 import json
+import time
 
 from datasets import Dataset
 from sentence_transformers import SentenceTransformer, util
 import torch
-from ir_measures import *
-import ir_measures
 
 import config
 from data import MSMarcoDocs, TREC2019, TREC2020
 import encoding
 import preprocessing
 
-from tqdm import tqdm
-
-
-METRICS = [nDCG, RR, AP, P @ 1, P @ 5, P @ 20, P @ 100, R @ 100]
 
 def rank(query: dict, docs: Dataset, model: SentenceTransformer) -> dict:
     scores, retrieved_docs = docs.get_nearest_examples(
@@ -33,26 +28,36 @@ def to_trec(query_id: str, ranking: dict, name: str) -> str:
         result += f"{query_id}\t0\t{doc_id}\t{i+1}\t{score}\t{name}\n"
     return result
 
-
-def evaluate(run, qrels_path: str, metrics: list) -> dict:
-    if isinstance(run, str):
-        run = ir_measures.read_trec_run(run)
-    qrels = ir_measures.read_trec_qrels(qrels_path)
-    return ir_measures.calc_aggregate(metrics, qrels, run)
-
 def main():
 
+	# Load model
+	device = "cuda" if torch.cuda.is_available() else "cpu"
+	model = SentenceTransformer("sentence-transformers/msmarco-distilbert-dot-v5").to(device)
+
 	# load data
-    docs = Dataset.load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model/')
-    print(docs[0]['passages'][0]['passage_id'])
-    print(docs[0])
- #    passages = docs['passages']
-	# # build faiss index
- #    try:
- #    	passages.load_faiss_index("embedding", "./data/ms-marco/passage-embeddings/passage-embeddings.faiss")
- #    except:
-	#     passages.add_faiss_index(column="embedding")
-	#     passages.save_faiss_index("embedding", "./data/ms-marco/passage-embeddings/passage-embeddings.faiss")
+	try:
+		passages = load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened/')
+	except:
+	    docs = Dataset.load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model/')    
+	    # flatten data
+	    ps_data = {
+	    	'passage_id': [],
+	    	'passage_embedding': []
+	    }
+	    for doc in docs:
+			for i, p_id in enumerate(doc['passages']['passage_id']):
+				ps_data['passage_id'].append(p_id)
+				ps_data['passage_embedding'].append(doc['passages']['passage_embedding'][i])
+
+		passages = Dataset.from_dict(ps_data)
+		passages.save_to_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened/')
+
+	# build faiss index
+    try:
+    	passages.load_faiss_index("passage_embedding", "./data/ms-marco/passage-embeddings/passage-embeddings.faiss")
+    except:
+	    passages.add_faiss_index(column="passage_embedding")
+	    passages.save_faiss_index("passage_embedding", "./data/ms-marco/passage-embeddings/passage-embeddings.faiss")
 
  #    # # Load queries (ds) and qrels (trec)
  #    # ms_marco_docs = MSMarcoDocs()
@@ -60,23 +65,56 @@ def main():
  #    # queries_msmarco = Dataset.load_from_disk('./data/ms-marco/query-embeddings/dev/')
  #    # qrels_msmarco = "./data/ms-marco/msmarco-docdev-qrels.tsv"
 
- #    # Load queries and qrels
- #    trec19 = TREC2019()
- #    queries_trec19 = trec19.get_queries()["test"]
- #    qrels_trec19 = "./data/trec/2019qrels-docs.tsv"
- #    # Rank
- #    name = args.dataset_file.split("/")[3] + "-trec19-ranking"
- #    result = "./data/results/" + name + ".tsv"
- #    with open(result, "w") as f:
- #        for i, query in enumerate(queries_trec19):
- #            if i % 100 == 0:
- #                print(f"Done {i}/{len(queries_trec19)} queries.")
- #            ranking = rank(query, docs, model)["ranking"]
- #            f.write(to_trec(query["query_id"], ranking, name))
+    # Load queries and qrels
+    trec19 = TREC2019()
+    queries_trec19 = trec19.get_queries()["test"]
+    qrels_trec19 = "./data/trec/2019qrels-docs.tsv"
+    # Rank
+    name = "passages-trec19-ranking"
+    results_max = []
+    results_mean = []
+    results_sum = []
+    a = time.time()
+    for i, query in enumerate(queries_trec19):
+    	b = time.time()
+        print(f"Done {i}/{len(queries_trec19)} queries in {b-a} sec")
+        ranking = rank(query, docs, model)["ranking"]
+        # Rerank
+		scores = {}
+		for p_id in ranking.keys():
+			d_id = p_id.split('_')[0]
+			i = ds['doc_id'].index(d_id)
+			doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in ds['passages'][i]['passage_embedding']])
+			scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
 
-    # Rerank mean
+		rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
+		rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
 
-    # Rerank sum
+        results_max.append((query["query_id"], ranking, name+'max'))
+        results_mean.append((query["query_id"], rerank_mean, name+'mean'))
+        results_sum.append((query["query_id"], rerank_sum, name+'sum'))
+
+    result_file = "./data/results/" + name + "max.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_max:
+            f.write(to_trec(query_id, ranking, name))
+
+    result_file = "./data/results/" + name + "mean.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_mean:
+            f.write(to_trec(query_id, ranking, name))
+
+    result_file = "./data/results/" + name + "sum.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_sum:
+            f.write(to_trec(query_id, ranking, name))
+
+
 
 if __name__ == "__main__":
 	main()
+
+
+
+
+
