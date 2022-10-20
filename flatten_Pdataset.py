ece7cddbf15ef86141d3c4d3ee95ca455a80458a
Thijs Sluijter <thijssluijter@MacBook-Pro-van-Thijs.local>
2022-10-20 09:49:21 +0200

    plz

--- flatten_Pdataset.py
+++ flatten_Pdataset.py
@@ -13,47 +13,59 @@ from data import MSMarcoDocs, TREC2019, TREC2020
 import encoding
 import preprocessing
 
+num_shards = args.n_shards
+k = args.fold_k
+
+START = int((k/num_shards)*3201821)
+END = int((k+1/num_shards)*3201821)
+
 def main():
 
-    # Load model
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"using device {device}")
+    print("===>STARTING SHARTING")
+    docs = Dataset.load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model/')
+    print('loaded data')
 
-    # load data
-    try:
-        passages = load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened/')
-    except:
-        print("===>STARTING SHARTING")
-        num_shards = 30
-        for k in range(1,num_shards):
-            print(f"SHARD {k}/{num_shards}")
-            # load only shard
-            docs = Dataset.from_dict(
-                    Dataset.load_from_disk(
-                        './data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model/'
-                    )[int((k/num_shards)*3201821):int((k+1/num_shards)*3201821)]
-                )
-            # flatten data
-            ps_data = {
-                'passage_id': [],
-                'passage_embedding': []
-            }
-            a = time.time()
-            for i, doc in enumerate(docs):
-                if i % 100 == 0:
-                    b = time.time()
-                    print(f"Flattening doc {i} out of {len(docs)}; time {b-a}")
-                for passage in doc['passages']:
-                    ps_data['passage_id'].append(passage['passage_id'])
-                    ps_data['passage_embedding'].append(passage['passage_embedding'])
+    # flatten data
+    ps_data = {
+        'passage_id': [],
+        'passage_embedding': []
+    }
+    a = time.time()
+    for i, doc in enumerate(docs):
+        if i < START:
+            pass
+        else:
+            if i % 100 == 0:
+                b = time.time()
+                print(f"Flattening doc {i} out of {len(docs)}; time {b-a}")
+            for passage in doc['passages']:
+                ps_data['passage_id'].append(passage['passage_id'])
+                ps_data['passage_embedding'].append(passage['passage_embedding'])
+            if i > END:
+                break
 
-            passages = Dataset.from_dict(ps_data)
-            passages.save_to_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened_{}/'.format(k))
-            print('Saved flattened passage dataset to disk')
+    passages = Dataset.from_dict(ps_data)
+    passages.save_to_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened_{}/'.format(k))
+    print('Saved flattened passage dataset to disk')
 
 
 
 if __name__ == "__main__":
+    argparser = ArgumentParser()
+    argparser.add_argument(
+        "-n",
+        "--n-shards",
+        type=int,
+        help="Number of Shards Total",
+    )
+    argparser.add_argument(
+        "-k",
+        "--fold-k",
+        type=int,
+        help="Which shard to compute",
+    )
+    args = argparser.parse_args()
+    main(args)
     main()
 
 
