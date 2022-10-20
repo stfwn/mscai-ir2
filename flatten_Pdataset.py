cc708d34aca11bf4c6ee716f1cee91db8f898784
Thijs Sluijter <thijssluijter@MacBook-Pro-van-Thijs.local>
2022-10-20 11:36:46 +0200

    .

--- flatten_Pdataset.py
+++ flatten_Pdataset.py
@@ -14,14 +14,15 @@ from data import MSMarcoDocs, TREC2019, TREC2020
 import encoding
 import preprocessing
 
+NUM_DOCS = 3201821
 
 def main(args):
 
     num_shards = args.n_shards
     k = args.fold_k
 
-    START = int((k/num_shards)*3201821)
-    END = int(((k+1)/num_shards)*3201821)
+    START = int(k/num_shards*NUM_DOCS)
+    END = int((k+1)/num_shards*NUM_DOCS)
 
     print("===>STARTING SHARTING")
     docs = Dataset.load_from_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model/')
@@ -33,22 +34,18 @@ def main(args):
         'passage_embedding': []
     }
     a = time.time()
-    for i in range(3201821):
-        if i < START:
-            pass
-        else:
-            doc = docs[i]
-            if i % 100 == 0:
-                b = time.time()
-                print(f"Flattening doc {i} out of {len(docs)}; time {b-a}")
-            for passage in doc['passages']:
-                ps_data['passage_id'].append(passage['passage_id'])
-                ps_data['passage_embedding'].append(passage['passage_embedding'])
-            if i >= END:
-                passages = Dataset.from_dict(ps_data)
-                passages.save_to_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened_{}/'.format(k))
-                print('Saved flattened passage dataset to disk')
-                break
+    for i in range(START, END):
+        doc = docs[i]
+        if i % 100 == 0:
+            b = time.time()
+            print(f"Flattening doc {i} out of {len(docs)}; time {b-a}")
+        for passage in doc['passages']:
+            ps_data['passage_id'].append(passage['passage_id'])
+            ps_data['passage_embedding'].append(passage['passage_embedding'])
+
+    passages = Dataset.from_dict(ps_data)
+    passages.save_to_disk('./data/ms-marco/passage-embeddings/passage_size=512+prepend_title_to_passage=True+tokenization_method=model+flattened_{}/'.format(k))
+    print('Saved flattened passage dataset to disk')
 
 
 
