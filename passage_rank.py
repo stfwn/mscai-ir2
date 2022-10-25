b04ec731e5d14e8c057de116f8e237fb43536a8b
Thijs Sluijter <thijssluijter@re-byods-146-50-206-116.wireless.uva.nl>
2022-10-25 16:13:01 +0200

    .

--- passage_rank.py
+++ passage_rank.py
@@ -60,43 +60,86 @@ def main():
         passages.save_faiss_index("passage_embedding", "./data/ms-marco/passage-embeddings/passage-embeddings.faiss")
         print('Saved faiss index to disk')
         
-    print('got this far')
-    print(passages.get_nearest_examples("passage_embedding", model.encode("hello world"), k=10))
-    # # Load queries and qrels
-    # trec19 = TREC2019()
-    # queries_trec19 = trec19.get_queries()["test"]
-    # qrels_trec19 = "./data/trec/2019qrels-docs.tsv"
-    # print('loaded queries and qrels')
-    # # Rank
-    # name = "passages-trec19-ranking"
-    # print('writing to {}'.format(name))
-    # results_max = []
-    # results_mean = []
-    # results_sum = []
-    # a = time.time()
-    # for i, query in enumerate(queries_trec19):
-    #     b = time.time()
-    #     if i % 10 == 0:
-    #         print(f"Done {i}/{len(queries_trec19)} queries in {b-a} sec")
-    #     ranking = rank(query, passages, model)["ranking"]
-    #     # Rerank
-    #     # scores = {}
-    #     # for p_id in ranking.keys():
-    #     #     d_id = p_id.split('_')[0]
-    #     #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
-    #     #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
+    # Load queries and qrels
+    trec19 = TREC2019()
+    queries_trec19 = trec19.get_queries()["test"]
+    qrels_trec19 = "./data/trec/2019qrels-docs.tsv"
+    print('loaded queries and qrels')
+    # Rank
+    name = "passages-trec19-ranking"
+    print('writing to {}'.format(name))
+    results_max = []
+    results_mean = []
+    results_sum = []
+    a = time.time()
+    for i, query in enumerate(queries_trec19):
+        b = time.time()
+        if i % 10 == 0:
+            print(f"Done {i}/{len(queries_trec19)} queries in {b-a} sec")
+        ranking = rank(query, passages, model)["ranking"]
+        # Rerank
+        # scores = {}
+        # for p_id in ranking.keys():
+        #     d_id = p_id.split('_')[0]
+        #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
+        #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
 
-    #     # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
-    #     # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
+        # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
+        # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
 
-    #     results_max.append((query["query_id"], ranking, name+'max'))
-    #     # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
-    #     # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
+        results_max.append((query["query_id"], ranking, name+'max'))
+        # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
+        # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
 
-    # result_file = "./data/results/" + name + "max.tsv"
-    # with open(result_file, "w") as f:
-    #     for query_id, ranking, name in results_max:
-    #         f.write(to_trec(query_id, ranking, name))
+    result_file = "./data/results/" + name + "max.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_max:
+            f.write(to_trec(query_id, ranking, name))
+
+    result_file = "./data/results/" + name + "mean.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_mean:
+            f.write(to_trec(query_id, ranking, name))
+
+    result_file = "./data/results/" + name + "sum.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_sum:
+            f.write(to_trec(query_id, ranking, name))
+
+
+    Load queries and qrels
+    trec20 = TREC2020()
+    queries_trec20 = trec20.get_queries()["test"]
+    qrels_trec20 = "./data/trec/2020qrels-docs.tsv"
+    # Rank
+    name = "passages-trec20-ranking"
+    results_max = []
+    results_mean = []
+    results_sum = []
+    a = time.time()
+    for i, query in enumerate(queries_trec20):
+        b = time.time()
+        if i % 10 == 0:
+            print(f"Done {i}/{len(queries_trec20)} queries in {b-a} sec")
+        ranking = rank(query, passages, model)["ranking"]
+        # Rerank
+        # scores = {}
+        # for p_id in ranking.keys():
+        #     d_id = p_id.split('_')[0]
+        #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
+        #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
+
+        # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
+        # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
+
+        results_max.append((query["query_id"], ranking, name+'max'))
+        # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
+        # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
+
+    result_file = "./data/results/" + name + "max.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_max:
+            f.write(to_trec(query_id, ranking, name))
 
     # result_file = "./data/results/" + name + "mean.tsv"
     # with open(result_file, "w") as f:
@@ -109,85 +152,40 @@ def main():
     #         f.write(to_trec(query_id, ranking, name))
 
 
-    # Load queries and qrels
-    # trec20 = TREC2020()
-    # queries_trec20 = trec20.get_queries()["test"]
-    # qrels_trec20 = "./data/trec/2020qrels-docs.tsv"
-    # # Rank
-    # name = "passages-trec20-ranking"
-    # results_max = []
+    # Load queries (ds) and qrels (trec)
+    ms_marco_docs = MSMarcoDocs()
+    queries_msmarco = ms_marco_docs.get_queries()["dev"]
+    # queries_msmarco = Dataset.load_from_disk('./data/ms-marco/query-embeddings/dev/')
+    qrels_msmarco = "./data/ms-marco/msmarco-docdev-qrels.tsv"
+    # Rank
+    name = "passages-ms-marco-ranking"
+    results_max = []
     # results_mean = []
     # results_sum = []
-    # a = time.time()
-    # for i, query in enumerate(queries_trec20):
-    #     b = time.time()
-    #     if i % 10 == 0:
-    #         print(f"Done {i}/{len(queries_trec20)} queries in {b-a} sec")
-    #     ranking = rank(query, passages, model)["ranking"]
-    #     # Rerank
-    #     # scores = {}
-    #     # for p_id in ranking.keys():
-    #     #     d_id = p_id.split('_')[0]
-    #     #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
-    #     #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
+    a = time.time()
+    for i, query in enumerate(queries_msmarco):
+        b = time.time()
+        if i % 100 == 0:
+            print(f"Done {i}/{len(queries_msmarco)} queries in {b-a} sec")
+        ranking = rank(query, passages, model)["ranking"]
+        # Rerank
+        # scores = {}
+        # for p_id in ranking.keys():
+        #     d_id = p_id.split('_')[0]
+        #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
+        #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
 
-    #     # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
-    #     # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
+        # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
+        # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
 
-    #     results_max.append((query["query_id"], ranking, name+'max'))
-    #     # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
-    #     # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
+        results_max.append((query["query_id"], ranking, name+'max'))
+        # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
+        # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
 
-    # result_file = "./data/results/" + name + "max.tsv"
-    # with open(result_file, "w") as f:
-    #     for query_id, ranking, name in results_max:
-    #         f.write(to_trec(query_id, ranking, name))
-
-    # # result_file = "./data/results/" + name + "mean.tsv"
-    # # with open(result_file, "w") as f:
-    # #     for query_id, ranking, name in results_mean:
-    # #         f.write(to_trec(query_id, ranking, name))
-
-    # # result_file = "./data/results/" + name + "sum.tsv"
-    # # with open(result_file, "w") as f:
-    # #     for query_id, ranking, name in results_sum:
-    # #         f.write(to_trec(query_id, ranking, name))
-
-
-    # # Load queries (ds) and qrels (trec)
-    # ms_marco_docs = MSMarcoDocs()
-    # queries_msmarco = ms_marco_docs.get_queries()["dev"]
-    # # queries_msmarco = Dataset.load_from_disk('./data/ms-marco/query-embeddings/dev/')
-    # qrels_msmarco = "./data/ms-marco/msmarco-docdev-qrels.tsv"
-    # # Rank
-    # name = "passages-ms-marco-ranking"
-    # results_max = []
-    # # results_mean = []
-    # # results_sum = []
-    # a = time.time()
-    # for i, query in enumerate(queries_msmarco):
-    #     b = time.time()
-    #     if i % 100 == 0:
-    #         print(f"Done {i}/{len(queries_msmarco)} queries in {b-a} sec")
-    #     ranking = rank(query, passages, model)["ranking"]
-    #     # Rerank
-    #     # scores = {}
-    #     # for p_id in ranking.keys():
-    #     #     d_id = p_id.split('_')[0]
-    #     #     doc_scores = np.array([np.dot(query_embedding, passage_embedding) for passage_embedding in get_embeddings(d_id, passages)])
-    #     #     scores.update({d_id: (doc_scores.sum(), doc_scores.mean())})
-
-    #     # rerank_sum = {k: v[0] for k, v in sorted(scores.items(), key=lambda item: item[1][0], reverse=True)}
-    #     # rerank_mean = {k: v[1] for k, v in sorted(scores.items(), key=lambda item: item[1][1], reverse=True)}
-
-    #     results_max.append((query["query_id"], ranking, name+'max'))
-    #     # results_mean.append((query["query_id"], rerank_mean, name+'mean'))
-    #     # results_sum.append((query["query_id"], rerank_sum, name+'sum'))
-
-    # result_file = "./data/results/" + name + "max.tsv"
-    # with open(result_file, "w") as f:
-    #     for query_id, ranking, name in results_max:
-    #         f.write(to_trec(query_id, ranking, name))
+    result_file = "./data/results/" + name + "max.tsv"
+    with open(result_file, "w") as f:
+        for query_id, ranking, name in results_max:
+            f.write(to_trec(query_id, ranking, name))
 
 
 if __name__ == "__main__":
